e9fcf803bd18d67727e6a3eea14713a7b1563258
17Joako <joakopacomorales@gmail.com>
2026-05-14 20:37:33 -0300

    Agregue constructor a Expediente

--- SGE.Dominio/Expedientes/Expediente.cs
+++ SGE.Dominio/Expedientes/Expediente.cs
@@ -8,4 +8,14 @@ public class Expediente
     private Guid UsuarioUltimoCambio { get; set; }
     
     private EstadoExpediente Estado  { get; set; }
+
+    public Expediente(Guid id, CaratulaExpedientes caratula, DateTime fechaCracion, DateTime fechaUltimaModificacion, Guid usuarioUltimoCambio, EstadoExpediente estado)
+    {
+        Id = id;
+        Caratula = caratula;
+        FechaCracion = fechaCracion;
+        FechaUltimaModificacion = fechaUltimaModificacion;
+        UsuarioUltimoCambio = usuarioUltimoCambio;
+        Estado = estado;
+    }
 }
