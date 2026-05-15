065297a0f8de6a9f7a671c425783523d113481d6
17Joako <joakopacomorales@gmail.com>
2026-05-14 22:16:53 -0300

    Hice método ActualizarEstado

--- SGE.Dominio/Expedientes/Expediente.cs
+++ SGE.Dominio/Expedientes/Expediente.cs
@@ -11,11 +11,35 @@ public class Expediente
 
     public Expediente(Guid id, CaratulaExpedientes caratula, DateTime fechaCreacion, DateTime fechaUltimaModificacion, Guid usuarioUltimoCambio)
     {
-        this.Id = id;
-        this.Caratula = caratula;
+        this.Id = new Guid();
+        this.Caratula = new CaratulaExpedientes(caratula);
         this.FechaCreacion = fechaCreacion;
         this.FechaUltimaModificacion = fechaUltimaModificacion;
-        this.UsuarioUltimoCambio = usuarioUltimoCambio;
+        this.UsuarioUltimoCambio = new Guid();
         this.Estado = EstadoExpediente.RecienIniciado;
+    }
+
+    public bool ActualizarEstado (EtiquetaTramite? ultimaEtiqueta, Guid idUsuario)
+    {
+        if (ultimaEtiqueta == null)
+        {
+            this.setEstado(EstadoExpediente.RecienIniciado);
+        }
+        else if (ultimaEtiqueta == EtiquetaTramite.Resolucion)
+        {
+            this.setEstado(EstadoExpediente.ConResolucion);
+        }
+        else if (ultimaEtiqueta == EtiquetaTramite.PaseAEstudio)
+        {
+            this.setEstado(EstadoExpediente.ParaResolver);
+        }
+        else if (ultimaEtiqueta == EstadoExpediente.PaseAlArchivo)
+        {
+            this.setEstado(EstadoExpediente.Finalizado);
+        }
+        else
+        {
+            return false;
+        }
     }
 }
