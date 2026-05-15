79666c62dfa8fc3d36f74b8454db1f31611e946d
17Joako <joakopacomorales@gmail.com>
2026-05-14 21:06:56 -0300

    Cambié dos boludeces en expediente

--- SGE.Dominio/Expedientes/Expediente.cs
+++ SGE.Dominio/Expedientes/Expediente.cs
@@ -3,19 +3,19 @@ public class Expediente
 {
     private Guid Id { get; set; }
     private CaratulaExpedientes Caratula { get; set; }
-    private DateTime FechaCracion { get; set; }
+    private DateTime FechaCreacion { get; set; }
     private DateTime FechaUltimaModificacion { get; set; }
     private Guid UsuarioUltimoCambio { get; set; }
     
     private EstadoExpediente Estado  { get; set; }
 
-    public Expediente(Guid id, CaratulaExpedientes caratula, DateTime fechaCracion, DateTime fechaUltimaModificacion, Guid usuarioUltimoCambio, EstadoExpediente estado)
+    public Expediente(Guid id, CaratulaExpedientes caratula, DateTime fechaCreacion, DateTime fechaUltimaModificacion, Guid usuarioUltimoCambio)
     {
-        Id = id;
-        Caratula = caratula;
-        FechaCracion = fechaCracion;
-        FechaUltimaModificacion = fechaUltimaModificacion;
-        UsuarioUltimoCambio = usuarioUltimoCambio;
-        Estado = estado;
+        this.Id = id;
+        this.Caratula = caratula;
+        this.FechaCreacion = fechaCreacion;
+        this.FechaUltimaModificacion = fechaUltimaModificacion;
+        this.UsuarioUltimoCambio = usuarioUltimoCambio;
+        this.Estado = EstadoExpediente.RecienIniciado;
     }
 }
