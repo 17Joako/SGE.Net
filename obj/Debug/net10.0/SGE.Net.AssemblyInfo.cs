340f72018963803da39724be4a69870b6b2a0187
17Joako <joakopacomorales@gmail.com>
2026-05-15 15:24:14 -0300

    aaa

--- obj/Debug/net10.0/SGE.Net.AssemblyInfo.cs
+++ obj/Debug/net10.0/SGE.Net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8d88689ba11ecbc0bc3543fedfbdc4e831acf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76bf48426ed6a02951c05b1ed6d81f499ae9b93")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
