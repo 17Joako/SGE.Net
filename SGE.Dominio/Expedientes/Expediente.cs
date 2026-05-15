6eb4060bf3efbd05339f0e34f11fcfdc6e36efd0
TheRecaGamer11 <bautirecalt@hotmail.com>
2026-05-14 23:08:54 -0300

    berenjena explica mejor porfa thx

--- SGE.Dominio/Expedientes/Expediente.cs
+++ SGE.Dominio/Expedientes/Expediente.cs
@@ -42,4 +42,20 @@ public class Expediente
             return false;
         }
     }
+    public void CambiarEstado(EstadoExpediente nuevoEstado, Guid idUsuario)//preguntar mañana si es mejor un booleano retornado o una excepcion
+    {
+        bool encontreUsuario=false;
+        StreamReader reader = new StreamReader("ruta_del_archivo_de_estados.txt");
+        //preguntar como saber cual es el id
+        while ((!reader.EndOfStream)&&(!encontreUsuario))
+        {
+            if(reader.ReadLine()==idUsuario.ToString())
+            {
+                //deberia modificar los archivos
+                encontreUsuario=true;
+            }
+        }
+        
+        reader.Close();
+    }
 }
